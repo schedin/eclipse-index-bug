b333f133379b5e27f8ae312c6f64d6473892248c
Lennart Schedin <lennart.schedin@polystar.com>
2016-08-17 10:44:25 +0200

    Adding some more project files and a java file

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+import scala.tools.ant.ScalaTool;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello world!");
+		ScalaTool.class.getClass();
+	}
+}
